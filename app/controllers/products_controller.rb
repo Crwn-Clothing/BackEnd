47d074ca6671f83a7e026052e58ce656eb15f7bb
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-01-12 00:45:14 -0500

    product controller complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  def show
+    @product = Product.find_by[params[:id]]
+    render json: @product
+  end
 end
